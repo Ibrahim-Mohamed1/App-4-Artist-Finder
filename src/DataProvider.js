6eb8669a1d3fd81841ea105af43ebc500b8aae86
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-05-17 12:14:06 -0600

    artist finder finished

--- src/DataProvider.js
+++ src/DataProvider.js
@@ -11,11 +11,13 @@ class DataProvider extends Component {
     }
 
     getArtist = (artist)=> {
-        axios.get(`https://vschool-cors.herokuapp.com?url=https://tastedive.com/api/similar?q=${artist}&k=334753-IbrahimM-B36QZN4K`).then(res => {
+        axios.get(`https://vschool-cors.herokuapp.com?url=https://tastedive.com/api/similar?q=${artist}&k=${process.env.REACT_APP_KEY}`).then(res => {
             this.setState({
                 artist: res.data
             })
-        })
+        }).catch(function (error) { 
+            window.location.reload() 
+        });
     }
 
     render() {
